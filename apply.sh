6c171dfa3d932f9216d9866989715fcc603dbda3
Michael Finger <michael.finger@icytv.de>
2024-02-27 00:10:56 +0100

    Generation 54

--- apply.sh
+++ apply.sh
@@ -6,7 +6,7 @@ pushd ~/system-flake
 
 git diff -U *.nix
 sudo nixos-rebuild switch --flake $PWD
-gen=$(nixos-rebuild list-generations | grep current)
-git commit -am "$gen"
+gen=$(nixos-rebuild list-generations | grep current | awk '{print $1}')
+git commit -am "Generation $gen"
 
 popd
