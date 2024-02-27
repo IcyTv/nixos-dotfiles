d43dc67d9809b71fdbc07037d3539e9172b76ded
Michael Finger <michael.finger@icytv.de>
2024-02-27 16:16:04 +0100

    Added confirmation Prompt to apply script

--- apply.sh
+++ apply.sh
@@ -5,6 +5,14 @@ set -e
 pushd ~/system-flake
 
 git diff
+
+read -p "Continue (y/n)?" CONT
+echo
+if [[ ! "$CONT" =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
 git add .
 sudo nixos-rebuild switch --flake $PWD
 gen=$(nixos-rebuild list-generations | grep current | awk '{print $1}')
