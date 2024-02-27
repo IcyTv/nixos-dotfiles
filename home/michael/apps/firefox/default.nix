604543d0fb79298029fc80519037a5fbc3d18836
Michael Finger <michael.finger@icytv.de>
2024-02-27 21:02:00 +0100

    Generation 92

--- home/michael/apps/firefox/default.nix
+++ home/michael/apps/firefox/default.nix
@@ -7,7 +7,7 @@ let
     url = "https://addons.mozilla.org/firefox/downloads/file/4013533/ycs-${version}.xpi";
     sha256 = "sha256-q2NfeC1Cc7syvM5nANyrz0ac0cr6Rv/9+nGon/Ttg/s=";
 
-    meta = {};
+    meta = { };
   };
   pinned-gmail = pkgs.nur.repos.rycee.firefox-addons.buildFirefoxXpiAddon rec {
     pname = "pinned-gmail";
@@ -16,11 +16,21 @@ let
     url = "https://addons.mozilla.org/firefox/downloads/file/4132394/pinned_gmail-${version}.xpi";
     sha256 = "sha256-sHdqCfSHc8SR5iigbAqpfSjfxqSAJ9BkGsfFelHrhcs=";
 
-    meta = {};
+    meta = { };
   };
 
+  shared-extensions = with pkgs.nur.repos.rycee.firefox-addons;
+    [
+      ublock-origin
+      privacy-badger
+      profile-switcher
+      decentraleyes
+      translate-web-pages
+    ];
+
   # TODO gemini for google & optisearch
-in {
+in
+{
   programs.firefox = {
     enable = true;
     package = pkgs.firefox.override {
@@ -43,8 +53,9 @@ in {
       DisplayMenuBar = "default-off";
 
       ExtensionSettings = {
-        "*" = {
-          installation_mode = "allowed";
+        "gmail_panel@alejandrobrizuela.com.ar" = {
+          installation_mode = "normal_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4132394/pinned_gmail-2.4.0.xpi";
         };
       };
     };
@@ -55,33 +66,40 @@ in {
         name = "NixOS";
         isDefault = true;
 
-        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-          ublock-origin
-          privacy-badger
-          bypass-paywalls-clean
-          profile-switcher
-          bitwarden
-          duckduckgo-privacy-essentials
-          flagfox
-          languagetool
-          onetab
-          return-youtube-dislikes
-          scroll_anywhere
-          sponsorblock
-          tabliss
-          translate-web-pages
-          unpaywall
-          playback-speed
-          web-archives
-          rust-search-extension
-          wikiwand-wikipedia-modernized
-          buster-captcha-solver
-          decentraleyes
-          link-cleaner
+        # extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        #   ublock-origin
+        #   privacy-badger
+        #   bypass-paywalls-clean
+        #   profile-switcher
+        #   bitwarden
+        #   duckduckgo-privacy-essentials
+        #   flagfox
+        #   languagetool
+        #   onetab
+        #   return-youtube-dislikes
+        #   scroll_anywhere
+        #   sponsorblock
+        #   tabliss
+        #   translate-web-pages
+        #   unpaywall
+        #   playback-speed
+        #   web-archives
+        #   rust-search-extension
+        #   wikiwand-wikipedia-modernized
+        #   buster-captcha-solver
+        #   decentraleyes
+        #   link-cleaner
 
-          ycs
-          pinned-gmail
-        ];
+        #   ycs
+        #   pinned-gmail
+        # ];
+      };
+
+      yes = {
+        id = 1;
+        name = "Yes";
+
+        extensions = shared-extensions;
       };
     };
   };
