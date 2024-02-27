c8998c311f8964df1a107e9e18cdd80c874caa27
Michael Finger <michael.finger@icytv.de>
2024-02-28 00:05:59 +0100

    Add custom commit messages
    Hopefully I will remember to do these in the future
    and improve the commit log

--- apply.sh
+++ apply.sh
@@ -16,6 +16,8 @@ git add .
 sudo nixos-rebuild switch --flake $PWD
 gen=$(nixos-rebuild list-generations | grep current | awk '{print $1}')
 git commit -am "Generation $gen"
+git commit --amend
+
 git push
 
 popd
