0d1a95c83cea9f5f1f51c2ca98ce4e269ad27557
Michael Finger <michael.finger@icytv.de>
2024-02-28 13:24:06 +0100

    Update to Rolling-Release NixOS

--- flake.nix
+++ flake.nix
@@ -2,10 +2,12 @@
   description = "IcyTv's NixOS system configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      # url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
