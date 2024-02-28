b93c7c63a26412ce758ee7b562a7ca7bca208ea8
Michael Finger <michael.finger@icytv.de>
2024-02-28 12:27:35 +0100

    Adjust gammastep temperature/times

--- home/michael/apps/default.nix
+++ home/michael/apps/default.nix
@@ -27,10 +27,22 @@
     provider = "manual";
     latitude = 49.0;
     longitude = 8.4;
+
+    dawnTime = "8:00-8:45";
+    duskTime = "22:15-23:30";
+
     tray = true;
+
+    temperature = {
+      day = 6500;
+      night = 1800;
+    };
+
     settings = {
       general = {
         adjustment-method = "wayland";
+        brightness-day = "1.0";
+        brightness-night = "0.8";
       };
     };
   };
