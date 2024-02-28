3ce3b8a21e7813f3ec233b105324d6f26f181530
Michael Finger <michael.finger@icytv.de>
2024-02-28 13:43:06 +0100

    Enable Running AppImages

--- hosts/general.nix
+++ hosts/general.nix
@@ -92,6 +92,15 @@
     ];
   };
 
+  boot.binfmt.registrations.appimage = {
+    wrapInterpreterInShell = false;
+    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+    recognitionType = "magic";
+    offset = 0;
+    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+    magicOrExtension = ''\x7fELF....AI\x02'';
+  };
+
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
