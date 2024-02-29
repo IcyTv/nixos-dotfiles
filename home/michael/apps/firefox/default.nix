ef22941efab82b128f031bf07dd943392448fe12
Michael Finger <michael.finger@icytv.de>
2024-02-29 12:44:35 +0100

    Firefox: Change Settings + Add Nix Search Engines

--- home/michael/apps/firefox/default.nix
+++ home/michael/apps/firefox/default.nix
@@ -25,9 +25,75 @@ let
       privacy-badger
       decentraleyes
       translate-web-pages
+      i-dont-care-about-cookies
     ];
 
   # TODO gemini for google & optisearch
+
+  sharedSettings = {
+    # disable Studies
+    # disable Normandy/Shield [FF60+]
+    # Shield is a telemetry system that can push and test "recipes"
+    "app.normandy.api_url" = "";
+    "app.normandy.enabled" = false;
+
+    # personalized Extension Recommendations in about:addons and AMO [FF65+]
+    # https://support.mozilla.org/kb/personalized-extension-recommendations 
+    "browser.discovery.enabled" = false;
+    "browser.helperApps.deleteTempFileOnExit" = true;
+
+    "browser.newtabpage.activity-stream.default.sites" = "";
+    "browser.newtabpage.activity-stream.feeds.topsites" = true;
+    "browser.newtabpage.activity-stream.showSponsored" = false;
+    "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+    "browser.uitour.enabled" = false;
+
+    "geo.provider.use_gpsd" = false;
+    "geo.provider.use_geoclue" = false;
+
+    # HIDDEN PREF: disable recommendation pane in about:addons (uses Google Analytics)
+    "extensions.getAddons.showPane" = false;
+    # recommendations in about:addons' Extensions and Themes panes [FF68+]
+    "extensions.htmlaboutaddons.recommendations.enabled" = false;
+
+
+    "browser.aboutConfig.showWarning" = false;
+    "browser.bookmarks.addedImportButton" = false;
+    # extensions.activeThemeID
+    "layout.css.prefers-color-scheme.content-override" = "dark";
+    # "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+    "svg.context-properties.content.enabled" = true;
+
+    # disable updates (pretty pointless with nix)
+    "app.update.channel" = "default";
+
+    "browser.download.viewableInternally.typeWasRegistered.svg" = true;
+    "browser.download.viewableInternally.typeWasRegistered.webp" = true;
+    "browser.download.viewableInternally.typeWasRegistered.xml" = true;
+
+    "browser.search.region" = "DE";
+
+    "browser.shell.checkDefaultBrowser" = false;
+
+    "browser.tabs.loadInBackground" = true;
+
+    "browser.urlbar.quickactions.enabled" = false;
+    "browser.urlbar.quickactions.showPrefs" = false;
+    "extensions.webcompat.perform_ua_overrides" = true;
+
+    "print.print_footerleft" = "";
+    "print.print_footerright" = "";
+    "print.print_headerleft" = "";
+    "print.print_headerright" = "";
+
+    "privacy.donottrackheader.enabled" = true;
+
+    # Yubikey
+    "security.webauth.u2f" = true;
+    "security.webauth.webauthn" = true;
+    "security.webauth.webauthn_enable_softtoken" = true;
+    "security.webauth.webauthn_enable_usbtoken" = true;
+  };
 
 in
 {
@@ -45,6 +111,7 @@ in
     package = pkgs.firefox;
 
     policies = {
+      CaptivePortal = false;
       DisableTelemetry = true;
       DisableFirefoxStudies = true;
       EnableTrackingProtection = {
@@ -57,6 +124,21 @@ in
       DisplayBookmarksToolbar = "always";
       DisplayMenuBar = "default-off";
       DisableProfileImport = true;
+      OfferToSaveLogins = false; # Because we have bitwarden
+
+      UserMessaging = {
+        ExtensionRecommendations = false;
+        SkipOnboarding = true;
+        WhatsNew = false;
+        MoreFromFirefox = false;
+      };
+      NoDefaultBookmarks = false;
+      DontCheckDefaultBrowser = true;
+      DisableSystemAddonUpdate = true;
+      ExtensionUpdate = false;
+
+      DisableFeedbackCommands = true;
+      SearchEngines.Default = "Google";
     };
 
     profiles = {
@@ -87,18 +169,37 @@ in
           buster-captcha-solver
           decentraleyes
           link-cleaner
+          i-dont-care-about-cookies
 
           ycs
           pinned-gmail
         ];
 
-        settings = {
-          "browser.aboutConfig.showWarning" = false;
-          "browser.bookmarks.addedImportButton" = false;
-          # extensions.activeThemeID
-          "layout.css.prefers-color-scheme.content-override" = "dark";
-          # "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-          "svg.context-properties.content.enabled" = true;
+        settings = sharedSettings;
+
+        search.engines = {
+          "Nix Packages" = {
+            urls = [{
+              template = "https://search.nixos.org/packages";
+              params = [
+                { name = "type"; value = "packages"; }
+                { name = "query"; value = "{searchTerms}"; }
+              ];
+            }];
+
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@np" ];
+          };
+
+          "My NixOS" = {
+            urls = [{ template = "https://mynixos.com/search?q={searchTerms}"; }];
+            iconUpdateUrl = "https://mynixos.com/favicon.ico";
+            updateInterval = 24 * 60 * 60 * 1000 * 30; # every month
+            definedAliases = [ "@nix" "@opts" ];
+          };
+
+          "Bing".metaData.hidden = true;
+          "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
         };
       };
 
@@ -108,12 +209,7 @@ in
 
         extensions = shared-extensions;
 
-        settings = {
-          "browser.aboutConfig.showWarning" = false;
-          "browser.bookmarks.addedImportButton" = false;
-          # extensions.activeThemeID
-          "layout.css.prefers-color-scheme.content-override" = "dark";
-        };
+        settings = sharedSettings;
       };
     };
   };
